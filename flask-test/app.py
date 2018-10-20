f21a8e758a8b2002c6f30f008ac418a81fd333fe
vincentlunc <xiaopeng@live.unc.edu>
2018-10-20 03:28:46 -0400

    DB connection

--- flask-test/app.py
+++ flask-test/app.py
@@ -1,11 +1,8 @@
 #!/anaconda/envs/py36/bin/python3.6.5
 
 # credit to https://github.com/yosiasz/d3-python-flask-mysql/
-from flask import Flask, jsonify, render_template, redirect, url_for
+from flask import Flask, jsonify, render_template, redirect, url_for, g, Response, request
 from flask_cors import CORS
-from flask import g
-from flask import Response
-from flask import request
 import json
 import pymysql
 from . import passw
@@ -18,60 +15,43 @@ CORS(app)
 
 @app.before_request
 def db_connect():
-  g.conn = pymysql.Connect(host=passw.HOST,
+    g.conn = pymysql.Connect(host=passw.HOST,
                               user=passw.USER,
                               passwd=passw.PASS,
-                              db='scheduler')
-  g.cursor = g.conn.cursor()
-
-
-
+                              db='inls641-aws')
+    g.cursor = g.conn.cursor()
 
 @app.after_request
 def db_disconnect(response):
-  g.cursor.close()
-  g.conn.close()
-  return response
+    g.cursor.close()
+    g.conn.close()
+    return response
   
 @app.route('/')
 def home():
-  '''return render_template('index.html',  title = 'Projects')'''
-  return redirect(url_for('static', filename='index.html'))
+    '''return render_template('index.html',  title = 'Projects')'''
+    return redirect(url_for('static', filename='flight.html'))
 
-@app.route('/rooms', methods=['GET'])  
-def rooms():
-  g.cursor.execute('SELECT roomid, roomname from rooms')
-  rooms = g.cursor.fetchall()
-  return jsonify({'rooms': rooms.name})
-	
-@app.route("/names", methods=['GET'])
-def names():
-  g.cursor.execute('SELECT personid, firstname, lastname from persons')
-  result = g.cursor.fetchall()
-  data = json.dumps(result)
-  resp = Response(data, status=200, mimetype='application/json')
-  return resp
 
-@app.route("/sales", methods=['GET'])
-def sales():
-  g.cursor.execute('select year,age,sex,people from sales')
-  data = g.cursor.fetchall()  
-  sales_list=[]
-  for item in data:
-    i = {
-    'year':item[0],
-    'age':item[1],
-    'people':item[2]
-    }
-    sales_list.append(i)
-  data = json.dumps(sales_list)
-  resp = Response(data, status=200, mimetype='application/json')
-  return resp
-  '''return {'StatusCode':'200','Sales':sales_list}'''
-  
+@app.route("/flight", methods=['GET'])
+def flight():
+    g.cursor.execute('select id,ArrDelay from flight_2017 where OriginCityName = "Charlotte, NC" and DestCityName = "Phoenix, AZ" and DayOfMonth = "17"')
+    row_headers=[x[0] for x in g.cursor.description]
+    data = g.cursor.fetchall()  
+    json_data=[]
+    for item in data:
+        json_data.append(dict(zip(row_headers,item)))
+        # i = {
+        # 'Year':item[0],
+        # 'Quarter':item[1],
+        # 'Month':item[2],
+        # 'DayOfMonth':item[3]
+        # }
+        # flight_list.append(i)
+    data = json.dumps(json_data)
+    resp = Response(data, status=200, mimetype='application/json')
+    return resp
+
+
 if __name__ == '__main__':
     app.run(debug=True)
-    
-    '''  return jsonify(year=result[0],
-                 age=result[1],
-                 people=result[2])'''  
